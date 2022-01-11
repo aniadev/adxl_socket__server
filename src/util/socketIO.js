0388ad8bcc77a321c252a2980cb21f22d1cc1a19
phamhai-laptop <phamcong.hai.90@gmail.com>
2022-01-11 12:47:58 +0700

    test cors

--- src/util/socketIO.js
+++ src/util/socketIO.js
@@ -1,7 +1,12 @@
 const { uuid } = require("uuidv4");
 // socketio handler
 socketIO = (server) => {
-  const io = require("socket.io")(server);
+  const io = require("socket.io")(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
   io.on("connect", (socket) => {
     console.log(socket.id);
     socket.on("adxl", (data) => {
