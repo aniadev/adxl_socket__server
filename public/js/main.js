0b2aa0d3fa5f0b58a68fdde8f4af37aadc0e0892
phamhai-laptop <phamcong.hai.90@gmail.com>
2022-04-16 13:51:34 +0700

    add event to socketio

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,52 @@ console.log("adxl");
 // const SERVER_URI = "http://localhost:9000";
 const SERVER_URI = "https://anidroid.herokuapp.com/";
 
+// DOM handler
 var socket = io(SERVER_URI);
+
+var x_span = document.getElementById("x-value");
+var y_span = document.getElementById("y-value");
+var z_span = document.getElementById("z-value");
+var adxlCanvas = document.getElementById("adxl-cvs");
+var ctx = adxlCanvas.getContext("2d");
+const CVS_WIDTH = 500;
+const CVS_HEIGHT = 200;
+
+adxlCanvas.width = CVS_WIDTH;
+adxlCanvas.height = CVS_HEIGHT;
+
+function canvasDraw(ax, ay, az) {
+  // co dinh ax
+  ax = preset(ax);
+  ay = preset(ay);
+  az = preset(az);
+  //    ax = [-10 : 10]
+  console.log(ax);
+  let radius = 80;
+  let alphaX = (ax / 10 - 1) * (Math.PI / 2);
+  ctx.beginPath();
+  //   clear canvas
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0, 0, CVS_WIDTH, CVS_HEIGHT);
+  ctx.fillStyle = "#000";
+  ctx.moveTo(CVS_WIDTH / 2, CVS_HEIGHT / 2);
+  ctx.lineTo(
+    CVS_WIDTH / 2 + radius * Math.cos(alphaX),
+    CVS_HEIGHT / 2 + radius * Math.sin(alphaX)
+  );
+  ctx.stroke();
+}
+
+function preset(val) {
+  return val <= -10 ? -10 : val > 10 ? 10 : val;
+}
+
+function setValue(x, y, z) {
+  x_span.innerHTML = x;
+  y_span.innerHTML = y;
+  z_span.innerHTML = z;
+}
+
 socket.on("connect", () => {
   // either with send()
   socket.emit("status", `client connected`);
@@ -10,5 +55,8 @@ socket.on("connect", () => {
 });
 socket.on("adxl", (data) => {
   // either with send()
-  console.log(data);
+  //   console.log(data);
+  let { x, y, z } = data;
+  setValue(x, y, z);
+  canvasDraw(x, y, z);
 });
--- src/util/socketIO.js
+++ src/util/socketIO.js
@@ -1,24 +1,27 @@
-const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 // socketio handler
 socketIO = (server) => {
-  const io = require("socket.io")(server, {
+  const io = require('socket.io')(server, {
     cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
+      origin: '*',
+      methods: ['GET', 'POST'],
     },
   });
-  io.on("connect", (socket) => {
+  io.on('connect', (socket) => {
     console.log(socket.id);
-    socket.on("adxl", (data) => {
-      io.emit("adxl", data);
+    socket.on('adxl', (data) => {
+      io.emit('adxl', data);
     });
-    socket.on("status", () => {
+    socket.on('status', () => {
       let count = { count: io.engine.clientsCount };
-      io.emit("online", JSON.stringify(count));
+      io.emit('online', JSON.stringify(count));
     });
-    socket.on("disconnect", () => {
+    socket.on('disconnect', () => {
       let count = { count: io.engine.clientsCount };
-      io.emit("online", JSON.stringify(count));
+      io.emit('online', JSON.stringify(count));
+    });
+    socket.on('lightSwitch', (data) => {
+      io.emit('lightSwitch', data);
     });
   });
 };
